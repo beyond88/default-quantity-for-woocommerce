78f3a793a4a666ea50446825f01a30e072544c6f
Jouleslabs <muhin.cse.diu@gmail.com>
2020-07-12 16:49:07 +0600

    update code

--- default-quantity-for-woocommerce.php
+++ default-quantity-for-woocommerce.php
@@ -39,51 +39,6 @@ define( 'DQFWC_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
 define( 'DQFWC_PLUGIN_NAME', 'Default Quantity for WooCommerce' );
 
 /**
- * The code that runs during plugin activation.
- * This action is documented in includes/class-default-quantity-for-woocommerce-activator.php
- */
-// function activate_default_quantity_for_woocommerce() {
-// 	require_once plugin_dir_path( __FILE__ ) . 'includes/class-default-quantity-for-woocommerce-activator.php';
-// 	Default_Quantity_For_Woocommerce_Activator::activate();
-// }
-
-/**
- * The code that runs during plugin deactivation.
- * This action is documented in includes/class-default-quantity-for-woocommerce-deactivator.php
- */
-// function deactivate_default_quantity_for_woocommerce() {
-// 	require_once plugin_dir_path( __FILE__ ) . 'includes/class-default-quantity-for-woocommerce-deactivator.php';
-// 	Default_Quantity_For_Woocommerce_Deactivator::deactivate();
-// }
-
-// register_activation_hook( __FILE__, 'activate_default_quantity_for_woocommerce' );
-// register_deactivation_hook( __FILE__, 'deactivate_default_quantity_for_woocommerce' );
-
-/**
- * The core plugin class that is used to define internationalization,
- * admin-specific hooks, and public-facing site hooks.
- */
-// require plugin_dir_path( __FILE__ ) . 'includes/class-default-quantity-for-woocommerce.php';
-
-/**
- * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
- *
- * @since    1.0.0
- */
-// function run_default_quantity_for_woocommerce() {
-
-// 	$plugin = new Default_Quantity_For_Woocommerce();
-// 	$plugin->run();
-
-// }
-// run_default_quantity_for_woocommerce();
-
-
-/**
  * Min and Max for WooCommerce Start.
  *
  * @since 1.0.0
